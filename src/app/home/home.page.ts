f39f4903635ae7a5ad3fbe7ecdfacb177f8e5aee
Chana Collin <chana@shtibel.com>
2019-04-29 12:35:45 +0300

    version 3 - 29.4.19

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Component} from '@angular/core';
+import { InAppBrowser , InAppBrowserOptions } from '@ionic-native/in-app-browser/ngx';
 import { Globals } from '../../providers/globals/globals';
 import { DomSanitizer } from '@angular/platform-browser';
-import { ToastController} from '@ionic/angular';
+import { ToastController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { Network } from '@ionic-native/network/ngx';
+
+
 
 @Component({
   selector: 'app-home',
@@ -12,32 +14,35 @@ import { Network } from '@ionic-native/network/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   siteUrl:any;
-
   constructor(public iab: InAppBrowser,
               public globals:Globals,
               public sanitizer:DomSanitizer,
               public toast: ToastController,
               public splashScreen: SplashScreen,
-              public network: Network ) {
+              public network: Network,
+              public platform: Platform ) {
   }
-
+  
   ionViewWillEnter (){
-
-this.splashScreen.show();    
+    this.splashScreen.show();    
     if (this.network.type == 'none' ){
       console.log("not net");
       this.splashScreen.show();    
     }
     else{
       this.siteUrl = this.globals.siteUrl;
-      this.siteUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.siteUrl);
-      console.log("siteUrl",this.siteUrl);
+      const options: InAppBrowserOptions = {
+        toolbar: 'no',
+        location: 'no',
+        zoom: 'no'
+      };
+      const browser = this.iab.create(this.siteUrl,'_blank',options);
+      //this.siteUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.siteUrl);
+      console.log("siteUrl ggg",this.siteUrl);
     }
+    
   }
 
-
-
-
+  
 }
